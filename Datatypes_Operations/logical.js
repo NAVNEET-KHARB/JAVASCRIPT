1437e13965769b4b5f4b0e0d8c9a03e7e71c8f26
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-04 18:03:36 +0530

    operations

---
+++ Datatypes_Operations/logical.js
@@ -0,0 +1,6 @@
+let isLoggedIn = true;
+let isPaid = false;
+console.log(isLoggedIn && isPaid); // And
+console.log(isLoggedIn || isPaid); // Or
+console.log(isLoggedIn && !isPaid); // And + Not
+console.log(typeof isPaid);
---
+++ Datatypes_Operations/operations.js
@@ -0,0 +1,26 @@
+let score = 102;
+let bonus = 25;
+// Arithmetic Operations
+let add = score + bonus;
+let sub = score - bonus;
+let mult = score * bonus;
+let divi = score / bonus;
+let exp = score ** bonus;
+let mod = score % bonus;
+console.log("Add : ", add);
+console.log("Sub : ", sub);
+console.log("Mult : ", mult);
+console.log("Divi : ", divi);
+console.log("Exp : ", exp);
+console.log("Mod : ", mod);
+let number = 100;
+console.log("number : ", number);
+console.log("number++ : ", number++);
+console.log("++number : ", ++number);
+// Comparison Operations
+let num1 = 3;
+let num2 = 6;
+let num3 = 6;
+console.log(num1 >= num3);
+console.log(num1 != num2);
+console.log(num2 == num3);
