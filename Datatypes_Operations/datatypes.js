e4168fafc5de1e6c0a736fefad66358809a42294
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-04 17:22:49 +0530

    Datatypes & Variables

---
+++ Datatypes_Operations/datatypes.js
@@ -0,0 +1,30 @@
+/*
+Datatypes in JS :->
+Number
+String
+BigInt
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+let name = "Navneet";
+let age = 20;
+let married = false;
+let teaTypes = ["lemon tea", "orange tea"];
+let user = { name: "Navneet", lastName: "Kharb" };
+console.log(name);
+console.log(age);
+console.log(married);
+console.log(teaTypes);
+console.log(user);
+let age2 = age;
+console.log(age2);
+age = 21;
+console.log(age2);
+const userName = "Nav1234";
+console.log(userName);
+// Can't redefine
+// userName = "Nav12344";
+console.log(userName);
---
+++ Datatypes_Operations/printing.js
@@ -0,0 +1,4 @@
+console.log("Hello");
+process.stdout.write("Chai");
+process.stdout.write("Chai\n");
+console.table({ city: "New Delhi" });
