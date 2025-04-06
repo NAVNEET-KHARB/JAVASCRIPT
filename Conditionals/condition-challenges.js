d4a0892af593abbdb3b7145fb565c8982be5b5e7
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-06 16:51:59 +0530

    Conditionals

---
+++ Conditionals/condition-challenges.js
@@ -0,0 +1,36 @@
+// *** Check if a number is greater than other
+let num1 = 20;
+let num2 = 22;
+
+// if(num1>num2) console.log("Num1 is greater");
+// else if(num2>num1) console.log("Num2 is greater");
+// else console.log("Both are equal");
+
+// *** Check if a string is equal to another string
+
+let name1 = "Navneet";
+let name2 = "navneet";
+
+// if(name1 === name2) console.log("Yes!! both strings are equal");
+// else console.log("No!! both strings aren't equal");
+
+// *** Checking if a variable is a number or not
+
+let var1 = 12;
+
+// if(typeof var1 === 'number') console.log("Yes it is a number");
+// else console.log("No it isn't a number");
+
+// *** Check if a boolean value is true or not
+
+let isValue = true;
+
+// if(isValue) console.log("true");
+// else console.log("false");
+
+// *** Checking if an array is empty or not
+
+let arr = [];
+
+if(arr.length) console.log("Array is not empty");
+else console.log("Array is empty");
