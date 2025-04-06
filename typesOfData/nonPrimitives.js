50cfb6ed17c791d11ea1a4a1ed2e13271f73fb65
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-06 16:12:34 +0530

    non-primitives and type-conversion

---
+++ typesOfData/nonPrimitives.js
@@ -0,0 +1,38 @@
+const user = {
+    "full name" : "Navneet Kharb",
+    firstName : "Navneet",
+    isLoggedIn : true
+}
+
+user.lastName = "Kharb";
+user.firstName = "Mr. Navneet";
+
+// console.log(user);
+// console.log(user["full name"]);
+// console.log(user["isLoggedIn"]);
+// console.log(user.firstName);
+// console.log(user.lastName);
+// console.log(typeof user);
+
+let today = new Date();
+// console.log(today);
+
+// Array
+
+let anotherUser = ["Navneet",true];
+// console.log(anotherUser);
+// console.log(anotherUser[0]);
+
+// Type Conversion
+
+// console.log("1" + 1);
+// let isValue = true;
+// console.log(1 + isValue);
+
+let val1 = "2";
+let val2 = "2abc";
+console.log(Number(val1));
+console.log(Number(val2));
+console.log(typeof NaN);
+console.log(Number(null));
+console.log(Number(undefined));
