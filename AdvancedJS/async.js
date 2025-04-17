91932704a1720f9f455925c3ab72bdee6b5b8186
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-17 22:02:33 +0530

    async

---
+++ AdvancedJS/async.js
@@ -0,0 +1,11 @@
+console.log("chaiCode");
+
+function sayHello(){
+    console.log("I would like to say Hello!!");
+}
+
+setTimeout(()=>{
+    sayHello();
+},3000);
+
+for(let i = 0; i<5; i++) console.log(i);
