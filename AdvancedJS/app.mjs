3a185d9e16a987cc0f30cab8d947f82aa11b4b3f
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-19 03:54:40 +0530

    Module exports & imports

---
+++ AdvancedJS/app.mjs
@@ -0,0 +1,9 @@
+// default import
+import multiply from "./mathOperationsM.mjs";
+
+// named import
+import {add,subtract} from "./mathOperationsM.mjs";
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
---
+++ AdvancedJS/mathOperationsM.mjs
@@ -0,0 +1,11 @@
+export function add(a,b){
+    return a+b;
+}
+
+export let subtract = (a,b)=>{
+    return a-b;
+}
+
+export default function multiply(a,b){
+    return a*b;
+}
